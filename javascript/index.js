981aabf16350ecf0762023d3062dacf695409d25
Toby <curti024@csusm.edu>
2016-04-21 09:57:55 -0700

    fixed selector and added partial css

--- javascript/index.js
+++ javascript/index.js
@@ -7,31 +7,33 @@ $( document ).ready
     window.compute = function()
     {
       //grab em all
-    var balance = $("#balance").val();
-    var rate = $("#rate").val();
-    var term = $("#term").val();
-    var n = $(":selected").val();
-    var either = $(":selected").text();
+      var balance = $("#balance").val();
+      var rate = $("#rate").val();
+      var term = $("#term").val();
+      var n = $(".periodOpt:selected").val();
+      var either = $(".periodOpt:selected").text();
+        
+      // number of payments 
+      var numberOfPayments = term * n;
       
-    // number of payments 
-    var numberOfPayments = term * n;
-    
-    // monthly interest rate
-    var monthlyInterestRate = (rate / 100) / n;
-    
-    // compounded interest rate 
-    var compounded = Math.pow((1 + monthlyInterestRate), numberOfPayments);
-    
-    // interest quotient 
-    var interestQuotient = (monthlyInterestRate * compounded) / (compounded - 1);
-    
-    // final calculation 
-    var monthlyPayment = balance * interestQuotient; 
-          
-    var fin = Math.round(monthlyPayment * 100) / 100  
+      // monthly interest rate
+      var monthlyInterestRate = (rate / 100) / n;
       
-     $("#output").text("Your "+either+" payment will be $" + fin);
+      // compounded interest rate 
+      var compounded = Math.pow((1 + monthlyInterestRate), numberOfPayments);
       
+      // interest quotient 
+      var interestQuotient = (monthlyInterestRate * compounded) / (compounded - 1);
+      
+      // final calculation 
+      var monthlyPayment = balance * interestQuotient; 
+      
+      //round to two decimal places      
+      var fin = Math.round(monthlyPayment * 100) / 100  
+      
+      //output statement
+      $("#output").text("Your "+either+" payment will be $" + fin);
+        
     }
     
   }
