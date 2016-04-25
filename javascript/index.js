68cf3fcf94ded8e080edb0c7c588cbd0f0b9cc0a
Toby <curti024@csusm.edu>
2016-04-25 13:20:09 -0700

    fixed the layout of my button and added required fields as a way to validate the input

--- javascript/index.js
+++ javascript/index.js
@@ -1,40 +1,39 @@
-$( document ).ready
-(
-  function()
-  {
-    
-    
-    window.compute = function()
+$(document).ready(
+    function()
     {
-      //grab em all
-      var balance = $("#balance").val();
-      var rate = $("#rate").val();
-      var term = $("#term").val();
-      var n = $(".periodOpt:selected").val();
-      var either = $(".periodOpt:selected").text();
-        
-      // number of payments 
-      var numberOfPayments = term * n;
-      
-      // monthly interest rate
-      var monthlyInterestRate = (rate / 100) / n;
-      
-      // compounded interest rate 
-      var compounded = Math.pow((1 + monthlyInterestRate), numberOfPayments);
-      
-      // interest quotient 
-      var interestQuotient = (monthlyInterestRate * compounded) / (compounded - 1);
-      
-      // final calculation 
-      var monthlyPayment = balance * interestQuotient; 
-      
-      //round to two decimal places      
-      var fin = Math.round(monthlyPayment * 100) / 100  
-      
-      //output statement
-      $("#output").text("Your "+either+" payment will be $" + fin);
-        
+
+
+        window.compute = function()
+        {
+            //grab em all
+            var balance = $("#balance").val();
+            var rate = $("#rate").val();
+            var term = $("#term").val();
+            var n = $(".periodOpt:selected").val();
+            var either = $(".periodOpt:selected").text();
+
+            // number of payments 
+            var numberOfPayments = term * n;
+
+            // monthly interest rate
+            var monthlyInterestRate = (rate / 100) / n;
+
+            // compounded interest rate 
+            var compounded = Math.pow((1 + monthlyInterestRate), numberOfPayments);
+
+            // interest quotient 
+            var interestQuotient = (monthlyInterestRate * compounded) / (compounded - 1);
+
+            // final calculation 
+            var monthlyPayment = balance * interestQuotient;
+
+            //round to two decimal places      
+            var fin = Math.round(monthlyPayment * 100) / 100;
+
+            //output statement
+            $("#output").text("Your " + either + " payment will be $" + fin);
+
+        };
+
     }
-    
-  }
-); 
+);
